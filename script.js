66b3794601a7bec0eb680ecb80030fffb01184e0
douglas godois dos santos <doug_potter92@hotmail.com>
2023-06-25 17:08:46 -0300

    atualizando layout

--- script.js
+++ script.js
@@ -26,5 +26,77 @@ toggle.onclick = () => {
   };
 
  
+/* seção da configuração dos graficos  */
 
-  
+  const ctx = document.getElementById('myChart');
+  const ctx2 = document.getElementById('myChart2');
+
+  new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+      labels: ['Microsoft', 'Google', 'Nuvem','Análise de Negócios'],
+      datasets: [{
+        label: 'Ferramentas',
+     
+        data: [20, 10, 5, 10],
+        backgroundColor: [
+            'rgba(255,99,132,0.2)',
+            'rgba(54,162,235,0.2)',
+            'rgba(255,206,86,0.2)',
+            'rgba(75,192,192,0.2)',
+            
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54,162,235,1)',
+            'rgba(255,206,86,1)',
+            'rgba(75,192,192,1)',
+          
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive:true,
+      
+      },
+      elements: {
+        line: {
+          borderDash: [] // Remove o padrão de traço da linha
+        }
+      }
+    }
+  );
+
+/* segundo grafico */
+
+
+
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'abril','junho','julho','agosto','setembro','outubro','novembro','dezembro'],
+    datasets: [{
+      label: "Metas de Aprendizagem Mensal",
+      color: 'rgba(255,255,255)',
+      data: [5, 4, 2, 2,2,2,2,5,7,8,10,20],
+      backgroundColor:[
+        'rgb(255, 99, 132,1)',
+        'rgb(54, 162, 235,1)',
+        'rgb(255, 206, 86,1)',
+        'rgb(75, 192, 192,1)',
+        'rgb(153, 102, 255,1)',
+        'rgb(255, 99, 132,1)',
+        'rgb(54, 162, 235,1)',
+        'rgb(255, 206, 86,1)',
+        'rgb(75, 192, 192,1)',
+        'rgb(153, 102, 255,1)',
+        
+     ]
+    }]
+  },
+  options: {
+ responsive:true,
+  }
+});
